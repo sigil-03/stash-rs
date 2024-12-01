4be285fe6b005ea5d2886fc52eecdf165962c48a
sigil-03 <sigil@glyphs.tech>
2024-12-01 14:10:16 -0700

    initial command line parsing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Subcommand)]
+enum Command {
+    Add,
+    Remove,
+    Inspect,
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+    #[command(subcommand)]
+    command: Command,
+}
+
+pub trait Stash {
+    fn add(&self);
+    fn remove(&self);
+    fn inspect(&self);
+}
+
+struct TestStash {}
+
+impl Stash for TestStash {
+    fn add(&self) {
+        todo!("add item to stash");
+    }
+    fn remove(&self) {
+        todo!("remove item from stash");
+    }
+    fn inspect(&self) {
+        todo!("inspect item in stash");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let s = TestStash {};
+
+    match cli.command {
+        Command::Add => s.add(),
+        Command::Remove => s.remove(),
+        Command::Inspect => s.inspect(),
+    };
 }
